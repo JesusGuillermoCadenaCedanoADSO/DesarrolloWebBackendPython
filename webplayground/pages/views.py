8604fc7a3cdb24dd496c7ecc2997717b8548426e
JesusGuillermoCadenaCedanoADSO <cadena.jesus@gmail.com>
2025-01-31 17:02:05 -0500

    SECCIÓN 4.62 VISTAS CRUD CON CBV: CREATEVIEW. sección parcialmente avanzada.

---
+++ webplayground/pages/views.py
@@ -0,0 +1,33 @@
+#from django.shortcuts import render, get_object_or_404, get_list_or_404
+from .models import Page
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.urls import reverse, reverse_lazy
+from .models import Page
+
+
+class PageListView(ListView):
+    model = Page
+
+class PageDetailView(DetailView):
+    model = Page
+
+class PageCreate(CreateView):
+    model = Page
+    fields = ['title','content','order']
+
+    # def get_success_url(self):
+    #     return reverse('pages:pages')
+    success_url = reverse_lazy('pages:pages')
+    
+
+
+# Create your views here.
+# def pages(request):
+#     pages = get_list_or_404(Page)
+#     return render(request, 'pages/pages.html', {'pages':pages})
+
+# def page(request, page_id, page_slug):
+#     page = get_object_or_404(Page, id=page_id)
+#     return render(request, 'pages/page.html', {'page':page})
